e606bb590dcb405ebb175308b1aeef0164a34bfd
xavimrg <javier.martin.mr@gmail.com>
2024-07-16 10:03:56 +0200

    basic architecture redone

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { Component } from '@angular/core';
+import { ListChecklistComponent } from './checklist/views/list-checklist/list-checklist.component';
+import { ReportsListViewComponent } from './layout/dashboard/views/reports-list-view/reports-list-view.component';
+
 
 export const routes: Routes = [
+    { path: '', component: LayoutComponent, 
+        children:[
+            {path: '', component: ListChecklistComponent}, 
+            {path: "checklists", redirectTo: ''},
+            {path: "reports", component: ReportsListViewComponent}
+        ]
+    }
 
-    {path: "", component: DashboardComponent}
 ];
