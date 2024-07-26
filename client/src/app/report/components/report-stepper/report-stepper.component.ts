b5599900970e80b040cbebad1eb82f9c92e23a27
limematheuz <mribeiro17.info@gmail.com>
2024-07-26 10:14:21 +0200

    reportview fixed; reportTable implemented with its properties; each report rediret to its id

---
+++ client/src/app/report/components/report-stepper/report-stepper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-stepper',
+  standalone: true,
+  imports: [],
+  templateUrl: './report-stepper.component.html',
+  styleUrl: './report-stepper.component.scss'
+})
+export class ReportStepperComponent {
+
+}
--- client/src/app/report/report.routes.ts
+++ client/src/app/report/report.routes.ts
@@ -1,9 +1,9 @@
 import { Route, Routes } from "@angular/router";
-import { EditReportComponent } from "./views/edit-report/edit-report.component";
 import { ListReportComponent } from "./views/list-report/list-report.component";
+import { ReportStepperComponent } from "./components/report-stepper/report-stepper.component";
 
 
 export const REPORT_ROUTES: Route[] = [
     {path: '', component: ListReportComponent}, 
-    { path: 'report/:id', component: EditReportComponent }
+    { path: 'report/:id', component: ReportStepperComponent  }
 ]
