24c5471afdf6d8b0f74657869b67c60a1aeaa26b
Daniel Cueto <dcueto14.info@gmail.com>
2024-07-31 09:48:12 +0200

    changed api_url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     PRODUCTION: false,
-    API_URL: 'http://13.37.212.43/api'
+    API_URL: 'https://light-eyes-reports.live/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'http://13.37.212.43/api'
+    API_URL: 'https://light-eyes-reports.live/api'
 };
